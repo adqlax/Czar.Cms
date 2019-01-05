fb35a1f28030f2d4c68a8dcf82ef74ff4101dd25
adqlax <1695567061@qq.com>
2019-01-05 23:35:23 +0800

    项目初始化

---
+++ Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        protected readonly List<Content> contents;
+        public ContentController(IOptions<List<Content>> option) {
+            contents = option.Value;
+        }
+        public IActionResult Index()
+        {
+            //var content = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    content.Add(new Content()
+            //    {
+            //        Id = i,
+            //        title = $"{i}的标题",
+            //        content = $"{i}的内容",
+            //        status = 1,
+            //        add_time = DateTime.Now.AddDays(-i)
+            //    });
+            //}
+            //return View(new ContentViewModel() { Contents=content});
+            return View(new ContentViewModel() { Contents = contents  });
+        }
+    }
+}
